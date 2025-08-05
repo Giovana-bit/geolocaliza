7a90082e99da890b650d61b63027b7a10b7b42f5
Giovana Santos <120347155+Giovana-bit@users.noreply.github.com>
2025-08-05 20:27:59 -0300

    add new project

---
+++ App.js
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { styles } from "./style";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    
+    </View>
+  )
+}
